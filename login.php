de7dedebe509965c3137c59c41914ddff0263a7c
TheJoeCoder <joe@radialbog9.uk>
2020-07-02 19:12:47 +0100

    i made a typo

--- login.php
+++ login.php
@@ -32,7 +32,7 @@
   </head>
   <body class="text-center">
     <form class="form-signin">
-  <img class="mb-4" src="./img/ison_256.png" alt="" width="72" height="72">
+  <img class="mb-4" src="./img/icon_256.png" alt="" width="72" height="72">
   <h1 class="h3 mb-3 font-weight-normal">Please sign in</h1>
   <label for="inputEmail" class="sr-only">Email address</label>
   <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
