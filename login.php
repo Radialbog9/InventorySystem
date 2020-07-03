990f6678d037c095c9474a2df271033341c41d18
TheJoeCoder <joe@radialbog9.uk>
2020-07-03 11:02:53 +0100

    add a line break

--- login.php
+++ login.php
@@ -6,6 +6,7 @@
         <strong>Error: </strong>
         Cannot connect to database: ('.$database_conn->connect_errno.') '.$database_conn->connect_error.'
       </div>
+      <br>
     ';
   }
   if($_SERVER['REQUEST_METHOD'] === 'POST') {
